cee4503e0d3f4517835f9ba627593de6924b77bb
KifoPL <kifounaabi@gmail.com>
2021-06-21 13:21:54 +0200

    updated documentation

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # kifo
  NPM package to make the Kifo Clanker™ cleaner and easier to manage
+
+## Installation
+`npm install kifo` in your terminal, and that's it!
+
+> make sure you have [`npm`](https://www.npmjs.com/get-npm) installed.
+
+## Features
+> Main usage of this package is to help develop Kifo Clanker™. However, in the future there may be actually useful features that other bot developers could use.
+
+- `embed(body, title = null)` - converts string (`body`) with optional title to embed message.
--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const Discord = require("discord.js");
 
-exports.embed = function(body, title = null) {
+/**
+ * 
+ * @param {string} body Description of message embed.
+ * @param {string} title Title of message embed. Default: "Info:"
+ * @returns 
+ */
+exports.embed = function(body, title = "Info:") {
 
 	const date = new(Date.now())
 
 	const embedreply = new Discord.MessageEmbed();
 	embedreply.setColor('a039a0')
 	.setAuthor("Powered by Kifo Clanker™", null, `https://discord.gg/HxUFQCxPFp`)
-	.setTitle(`${title == null ? "INFO:" : title}`)
+	.setTitle(title)
 	.setURL("https://kifopl.github.io/kifo-clanker/")
 	.setDescription(body)
 	.setFooter(`by KifoPL, sent at: ${date.toUTCString()}.`)
